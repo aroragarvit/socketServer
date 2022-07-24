de280f67dab60526d514d00c39263ed57852cdc8
garvit <agarvit1142000@gmail.com>
2022-07-24 12:33:17 +0530

    Added functionality to join random games

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io"); 
+const { Server } = require("socket.io");
 const server = http.createServer(app);
 const cors = require("cors");
 app.use(cors());
@@ -13,58 +13,76 @@ server.listen(3001, () => {
   console.log("server is running on port 3001");
 });
 
-
 const clientRooms = {}; // socket id as key and room name as value
 const state = {};
-
-
+let availableRooms = [];
 
 io.on("connection", (socket) => {
   console.log(`a user connected: ${socket.id}`);
   socket.on("newGame", createNewGame);
-  socket.on("joinGame", joinGame);
 
   function createNewGame() {
-    let roomName = Math.random().toString(36).substring(2, 15);
-    clientRooms[socket.id] = roomName;
-    console.log("Socket id of creator:", socket.id);
-    socket.emit("gameCode", roomName); // emit game code 
-    socket.join(roomName);
-    socket.emit("connectToRoom", { "roomName": roomName , "socketId": socket.id});
-
-  }
-
-  function joinGame(roomName) {
-    console.log("Socket id of joiner:", socket.id);
-    clientSet=io.sockets.adapter.rooms.get(roomName);
-    let allUsers = clientSet.size
-    
-    
-    //
-    //  if (allUsers === 0) {
-    //   //client.emit('unknownCode');
-    //   console.log("unknownCode");
-    //   return;
-    // } else if (allUsers > 1) {
-    //   //client.emit('tooManyPlayers');
-    //   console.log("tooManyPlayers");
-    //   return;
-    // }
-    
-    clientRooms[socket.id] = roomName;
-
-    socket.join(roomName);
-    socket.emit("connectToRoom", { "roomName": roomName , "socketId": socket.id});
-    console.log(io.of("/").adapter);
-
-    // console.log("numClients", numClients);
-
-    // client.number = 2;
-    // client.emit('init', 2);
+    // Its like joining
+    if (availableRooms.length > 0) {
+      console.log("Available Rooms", availableRooms);
+      const roomName = availableRooms.pop();
+      socket.join(roomName);
+      a = Array.from(io.sockets.adapter.rooms.get(roomName));
+      console.log(`All ids in room${roomName} are ${a}`);
+      clientRooms[socket.id] = roomName;
+      socket.emit("connectToRoom", { roomName: roomName, socketId: socket.id }); // see for self
+      socket.broadcast.to(roomName).emit("playerJoined", socket.id); // to the person waiting in the room
+    } else {
+      // Its like creating
+      let roomName = Math.random().toString(36).substring(2, 15);
+      availableRooms.push(roomName);
+      socket.join(roomName);
+      clientRooms[socket.id] = roomName;
+      socket.emit("connectToRoom", { roomName: roomName, socketId: socket.id }); // see for self
+    }
   }
 });
 
-// run when the client connects
+//   let roomName = Math.random().toString(36).substring(2, 15);
+//   clientRooms[socket.id] = roomName;
+//   console.log("Socket id of creator:", socket.id);
+//   socket.emit("gameCode", roomName); // emit game code
+//   socket.join(roomName);
+//   socket.emit("connectToRoom", { roomName: roomName, socketId: socket.id });
+// }
+//
+// function joinGame(roomName) {
+//   console.log("Socket id of joiner:", socket.id);
+//   clientSet = io.sockets.adapter.rooms.get(roomName);
+//   let allUsers = clientSet.size;
+//
+//   if (allUsers === 0) {
+//     //client.emit('unknownCode');
+//     console.log("unknownCode");
+//     return;
+//   } else if (allUsers > 1) {
+//     //client.emit('tooManyPlayers');
+//     console.log("tooManyPlayers");
+//     return;
+//   }
+//
+//   clientRooms[socket.id] = roomName;
+//
+//   socket.join(roomName);
+//   socket.emit("connectToRoom", { roomName: roomName, socketId: socket.id });
+//   a = Array.from(io.sockets.adapter.rooms.get(roomName));
+//   console.log(a);
+//
+//   socket.broadcast.to(roomName).emit("playerJoined", socket.id);
+//   setInterval(function () {
+//     number = Math.floor(Math.random() * 100 + 1);
+//     socket.broadcast.to(roomName).emit("number", number);
+//     socket.nsp.to(roomName).emit("number", number + 1);
+//   }, 1000);
+// }
+//});
+//
+//// run when the client connects
 //io.on("connection", (socket) => {
 //  console.log(`a user connected: ${socket.id}`);
 //  socket.emit("Welcome", "welcome to the chat");
